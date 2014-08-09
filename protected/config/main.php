276644b7df515d591726a4edc18479deda7c3b01
Obodeh Samuel Egheose <s.obodeh@netowsolutions.net>
2014-08-09 22:47:17 +0100

    Fixed db permissions issue

--- protected/config/main.php
+++ protected/config/main.php
@@ -57,8 +57,8 @@ return array(
 		'db'=>array(
             'connectionString' => 'mysql:host=localhost;dbname=cog2014',
             'emulatePrepare' => true,
-            'username' => 'sam',
-            'password' => 'samtest',
+            'username' => 'root',
+            'password' => 'Egheose89?',
 			'charset' => 'utf8',
 		),
 
